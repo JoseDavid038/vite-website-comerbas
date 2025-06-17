33eb4f390e72ea990aa2d83df7994c7989a10924
Jose David Gonzalez <programacionjose2@gmail.com>
2025-06-17 12:59:50 -0500

    add dropdown menu in home.html mobile version

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     port: 5173, // You can change this to any port you like
     host: true, // important to allow external access
     allowedHosts: [
-      '57ae-186-29-36-159.ngrok-free.app' // <- your ngrok domain
+      '3615-190-27-104-237.ngrok-free.app' // <- your ngrok domain
     ]
   }
 });
