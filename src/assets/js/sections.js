30c4bc5e3f9fa15bfb8da186e63aa9042b0a6bee
Jose David Gonzalez <programacionjose2@gmail.com>
2025-07-12 16:13:38 -0500

    change format images png to webp

--- src/assets/js/sections.js
+++ src/assets/js/sections.js
@@ -39,7 +39,7 @@ async function loadComponent (selector, componentPath, data = {}) {
 
 
 loadComponent("#section-1", "components/sections.html", {
-  image: "images/services/service-optometria-test.png",
+  image: "images/services/service-optometria-test.webp",
   title: "Optometria laboral",
   paragraph: "Ofrecemos evaluaciones precisas y certificadas permitiéndole a su empresa tomar decisiones informadas y garantizar un entorno laboral seguro.",
   list: ["Evaluación completa de la agudeza visual lejana y cercana", "Pruebas de profundidad visual y discriminación de colores", "Recomendaciones específicas según el puesto de trabajo", "Prevención de riesgos visuales ocupacionales"],
@@ -49,7 +49,7 @@ loadComponent("#section-1", "components/sections.html", {
 
 
 loadComponent("#section-2", "components/section2.html", {
-  image: "images/services/service-psicology.png",
+  image: "images/services/service-psicology.webp",
   title: "Psicología Laboral",
   paragraph: "Evaluamos los factores psicosociales que impactan en el desempeño y bienestar de sus colaboradores, identificando fortalezas y áreas de mejora para optimizar el ambiente laboral.",
   list: ["Evaluación de Riesgos Bio-Psicosociales", "Pruebas Psicotécnicas Laborales", "Capacitación en Salud Mental Laboral", "Control de Sustancias psicoactivas y Alcoholimetría."],
@@ -58,7 +58,7 @@ loadComponent("#section-2", "components/section2.html", {
 });
 
 loadComponent("#section-3", "components/section3.html", {
-  image: "images/services/service-certified.png",
+  image: "images/services/service-certified.webp",
   title: "Certificados especializados para seguridad.",
   paragraph: "Evaluaciones completas para porte y tenencia de armas con resultados confiables y rápidos.",
   paragraph2: "Entendemos las necesidades específicas del sector de seguridad privada. Ofrecemos un servicio integral que cumple con todos los requisitos legales para la certificación de su personal, garantizando procesos ágiles y resultados precisos.",
@@ -67,7 +67,7 @@ loadComponent("#section-3", "components/section3.html", {
 });
 
 loadComponent("#section-4", "components/section4.html", {
-  image: "images/services/service-support-sg-sst.png",
+  image: "images/services/service-support-sg-sst.webp",
   title: "Asesoría e Implementación de Sistemas de Gestión SG-SST",
   paragraph: "Detectamos y evaluamos factores de riesgo críticos que pueden comprometer la seguridad operacional y el cumplimiento normativo, ofreciendo soluciones preventivas adaptadas a su sector.",
   buttonText: "Solicita una asesoria",
@@ -75,7 +75,7 @@ loadComponent("#section-4", "components/section4.html", {
 });
 
 loadComponent("#section-5", "components/section5.html", {
-  image: "images/services/service-labor-exam.jpg",
+  image: "images/services/service-labor-exam.webp",
   title: "Exámenes Médicos de Ingreso Completos y Certificados",
   paragraph: "Ofrecemos atención médica especializada con resultados rápidos y confiables.Nuestros profesionales realizan evaluaciones integrales que cumplen todas las normativas vigentes en salud ocupacional.",
   buttonText: "Conoce nuestra IPS",
@@ -83,15 +83,15 @@ loadComponent("#section-5", "components/section5.html", {
 });
 
 loadComponent("#section-6", "components/section5.html", {
-  image: "images/testimonials/mision.png",
-  title: "Nuestra Propósito",
+  image: "images/testimonials/mision.webp",
+  title: "Nuestro Propósito",
   paragraph: "Ofrecemos productos y servicios que mejoran la calidad de vida de los colaboradores y sus familias.",
   buttonText: "Contáctanos para saber más",
   buttonLink: "#contact-form"
 });
 
 loadComponent("#section-7", "components/section6.html", {
-  image: "images/services/service-exequial.png",
+  image: "images/services/service-exequial.webp",
   title: "Acompañamos a tu equipo en los momentos dificiles con planes exequiales",
   paragraph: "Brinda tranquilidad total a tu familia en los momentos más difíciles. Te acompañamos en los momentos más dificiles.",
   buttonText: "Contáctanos para saber más",
@@ -99,7 +99,7 @@ loadComponent("#section-7", "components/section6.html", {
 });
 
 loadComponent("#section-8", "components/section6.html", {
-  image: "images/services/service-dreams.png",
+  image: "images/services/service-dreams.webp",
   title: "Ayuda a tu equipo a cumplir sus sueños con nosotros",
   paragraph: "Ofrecemos un gran portafolio de créditos para que tus empleados puedan cumplir sus proyectos. Nosotros nos encargamos de todo.",
   buttonText: "Contáctanos para saber más",
