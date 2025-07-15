57598bee1fe2ae12d4e3a358021ecc4cd7fef63e
Jose David Gonzalez <programacionjose2@gmail.com>
2025-07-15 18:32:13 -0500

    update vite.config.js for production

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,38 @@
 import { defineConfig } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
-import { resolve } from 'path'
+import { resolve } from 'path';
 
+const deployTarget = process.env.DEPLOY_TARGET || 'local'; // local | github | hosting
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production'
-    ? '/vite-website-comerbas/' // para GitHub Pages
-    : '/', // para desarrollo local, 
-    build: {
+  base:
+    deployTarget === 'github'
+      ? '/vite-website-comerbas/'
+      : '/', // para hosting o local
+
+  build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
         empresas: resolve(__dirname, 'empresas.html'),
-        nosotros: resolve(__dirname, 'ips.html'),
-        contacto: resolve(__dirname, 'nosotros.html'),
+        nosotros: resolve(__dirname, 'nosotros.html'),
+        ips: resolve(__dirname, 'ips.html'),
         expectativa: resolve(__dirname, 'expectativa.html')
       }
     }
   },
-    plugins: [
+
+  plugins: [
     viteStaticCopy({
       targets: [
-        { src: 'public/*', dest: '' }, // para asegurarte de que archivos de public se copien si es necesario
+        { src: 'public/*', dest: '' },
       ]
     })
-    ],
-    server: {
-      port: 5173, // You can change this to any port you like
-      host: true, // important to allow external access
-      allowedHosts: [
-        '8e41d02f1dc5.ngrok-free.app' // <- your ngrok domain
-      ]
-    }
-  });
+  ],
+
+  server: {
+    port: 5173,
+    host: true,
+    allowedHosts: ['8e41d02f1dc5.ngrok-free.app']
+  }
+});
